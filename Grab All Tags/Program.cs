fe558441412ec9553aeb57d9a244ca9cf4b7d431
Ryan Ricardson <richa210@mail.chapman.edu>
2016-04-17 15:07:02 -0700

    Matched Git Naming, Added Path variables

---
+++ Grab All Tags/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KSP_Search_Tag_Scraper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, HashSet<string>> dictTags = new Dictionary<string, HashSet<string>>();
+            var kerbalDirectory = string.Empty;
+            var outputDirectory = string.Empty;
+
+            foreach (
+                string f in
+                    Directory.GetFiles(
+                        path: kerbalDirectory,
+                        searchPattern: "*.cfg", searchOption: SearchOption.AllDirectories))
+            {
+
+                string category = string.Empty;
+
+                foreach (var line in File.ReadAllLines(f))
+                {
+
+                    if (line.Contains("\tcategory ="))
+                    {
+                        category = line.Split('=')[1];
+                    }
+
+                    if (line.Contains("tags ="))
+                    {
+                        if (!dictTags.ContainsKey(category))
+                        {
+                            dictTags.Add(category, new HashSet<string>());
+                        }
+
+                        var catList = dictTags.FirstOrDefault(x => x.Key == category).Value;
+
+                        string[] nTags = line.Substring(7).Split(null);
+                        foreach (var tag in nTags)
+                        {
+                            if (!string.IsNullOrWhiteSpace(tag))
+                            {
+                                catList.Add(tag);
+
+                            }
+                        }
+
+                    }
+                }
+
+            }
+
+            using (StreamWriter newFile = new StreamWriter(outputDirectory))
+            {
+
+                foreach (var outTag in dictTags)
+                {
+                    newFile.WriteLine(outTag.Key);
+
+                    foreach (var uniqueTag in outTag.Value)
+                    {
+                        newFile.WriteLine("\t" + uniqueTag);
+                    }
+                }
+            }
+
+        }
+    }
+}
+
---
+++ Grab All Tags/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Grab All Tags")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Grab All Tags")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("aa866fbe-8b34-4113-9d24-04c23e974c7a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
