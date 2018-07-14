72a354d02a0c78a74dc0e0646cbfeab5a6bd9153
sparkybear <ryanr.spark@gmail.com>
2018-07-13 17:45:24 -0700

    Refactored tag storage
    Converted from a hash set to a list of strings to allow for automatic sorting of tags. Added in an additional filter to remove the #auto tags added with the new updates. Also made some other updates and changes to variable names.

--- KSP-Search-Tag-Scraper/Program.cs
+++ KSP-Search-Tag-Scraper/Program.cs
@@ -12,20 +12,18 @@ namespace KSP_Search_Tag_Scraper
     {
         static void Main(string[] args)
         {
-            Dictionary<string, HashSet<string>> dictTags = new Dictionary<string, HashSet<string>>();
-            var kerbalDirectory = string.Empty;
-            var outputDirectory = string.Empty;
+            var dictTags = new Dictionary<string, List<string>>();
+            var kerbalDirectory = @"D:\Steam\steamapps\common\Kerbal Space Program\GameData\Squad";
+            var outputDirectory = @"C:\Users\Ryan\Desktop";
 
+            var category = string.Empty;
             foreach (
-                string f in
+                var file in
                     Directory.GetFiles(
                         path: kerbalDirectory,
                         searchPattern: "*.cfg", searchOption: SearchOption.AllDirectories))
             {
-
-                string category = string.Empty;
-
-                foreach (var line in File.ReadAllLines(f))
+                foreach (var line in File.ReadAllLines(file))
                 {
 
                     if (line.Contains("\tcategory ="))
@@ -33,31 +31,27 @@ namespace KSP_Search_Tag_Scraper
                         category = line.Split('=')[1];
                     }
 
-                    if (line.Contains("tags ="))
+                    if (!line.Contains("tags =")) continue;
+                    if (!dictTags.ContainsKey(category))
                     {
-                        if (!dictTags.ContainsKey(category))
-                        {
-                            dictTags.Add(category, new HashSet<string>());
-                        }
-
-                        var catList = dictTags.FirstOrDefault(x => x.Key == category).Value;
-
-                        string[] nTags = line.Substring(7).Split(null);
-                        foreach (var tag in nTags)
-                        {
-                            if (!string.IsNullOrWhiteSpace(tag))
-                            {
-                                catList.Add(tag);
-
-                            }
-                        }
-
+                        dictTags.Add(category, new List<string>());
                     }
+
+                    var catList = dictTags.FirstOrDefault(x => x.Key == category).Value;
+
+                    var nTags = line.Substring(7).Split(null);
+                    foreach (var tag in nTags)
+                    {
+                        if (string.IsNullOrWhiteSpace(tag) || catList.Contains(tag)) continue;
+                        if (tag.Contains(@"#auto")) continue;
+                        catList.Add(tag);
+                    }
+                    catList.Sort(StringComparer.InvariantCultureIgnoreCase);
                 }
 
             }
 
-            using (StreamWriter newFile = new StreamWriter(outputDirectory+ "\\KSPTags.txt"))
+            using (var newFile = new StreamWriter(outputDirectory+ "\\KSPTags.txt"))
             {
 
                 foreach (var outTag in dictTags)
@@ -68,6 +62,8 @@ namespace KSP_Search_Tag_Scraper
                     {
                         newFile.WriteLine("\t" + uniqueTag);
                     }
+
+                    newFile.WriteLineAsync("\n");
                 }
             }
 
